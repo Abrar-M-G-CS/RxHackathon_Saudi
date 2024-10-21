257dc71ee65d06444b61ff3cdd93f61987b0affa
Abrar Ghandurah <80005788+Abrar-M-G-CS@users.noreply.github.com>
2024-10-21 09:38:03 +0300

    Initial Commit

---
+++ RXHackthon/RXHackthonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RXHackthonApp.swift
+//  RXHackthon
+//
+//  Created by Abrar Ghandurah on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RXHackthonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
